5725188b7468aea91136da7d9556ee3e4de3bd5e
Nelson <neldecas12@gmail.com>
2023-03-04 16:14:05 -0500

    .

--- src/page/Management/Roles/Roles.tsx
+++ src/page/Management/Roles/Roles.tsx
@@ -7,17 +7,10 @@ import type { RootState } from "@/service/context/app/store"
 
 export const Roles = () => {
 
-    const flatData: any[] = [];
 
-    const { state } = GetAll<Person>("user/");
-    const isLoad = useSelector((state: RootState) => state.store.isLoad)
-
-    state?.map((user) => {
-        flatData.push({ ...user, roles: user.roles.name })
-    })
 
 
     return (
-        <Table data={flatData} visible_fields={["id", "username", "roles", "email"]} load={isLoad} />
+        <div>h</div>
     )
 }
