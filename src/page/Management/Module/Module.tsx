4a8302811d0470a21531aa6f0c1fcd76b0243757
Nelson <neldecas12@gmail.com>
2023-03-04 13:11:10 -0500

    module table

--- src/page/Management/Module/Module.tsx
+++ src/page/Management/Module/Module.tsx
@@ -1,7 +1,38 @@
 import { Table } from "@/components/UI/table/Table";
+import { GetAll } from "@/service/hooks/GetAll";
+import { useSelector } from "react-redux";
+import type { RootState } from "@/service/context/app/store";
+
+
+interface Iresponse {
+    name: string;
+    categoria: string;
+    modulos_has_role: Array<any>;
+}
+
+interface Irol {
+    rol: Array<any>
+}
+
 
 export const Module = () => {
+    const { state } = GetAll<Iresponse>("/modulos");
+    type response = Iresponse & Irol;
+    let flatData: response[] = [];
+    const load = useSelector((s: RootState) => s.isLoad.isLoad)
+
+    state?.forEach((e) => {
+        e.modulos_has_role.forEach((c) => {
+            if (flatData.some((s) => s.name === c.name)) {
+                let index = flatData.findIndex((s) => s.name === c.name);
+                flatData[index].rol.push(e.name)
+            } else {
+                flatData.push({ ...c, rol: [e.name] })
+            }
+        })
+    });
+
     return (
-        <Table />
+        <Table data={flatData} visible_fields={["id", "name", "rol", "path", "dependencia"]} load={load} />
     )
 }
